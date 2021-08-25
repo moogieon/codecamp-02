4a3642559157fd0fbd8f52ea2a7f8185efc7df2f
moogieon <vzlzydn5007@gmail.com>
2021-08-25 13:42:37 +0900

    docker test

--- class_build/pages/_app.js
+++ class_build/pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  console.log(process.env.NEXT_PUBLIC_KAKAOMAP_KEY);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
