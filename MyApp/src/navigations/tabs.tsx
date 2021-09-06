cc44ee3224514317d78d50d0b876d44d2e0cd3b1
moogieon <vzlzydn5007@gmail.com>
2021-09-06 22:26:32 +0900

    react-native  백업 -3

--- MyApp/src/navigations/tabs.tsx
+++ MyApp/src/navigations/tabs.tsx
@@ -1,13 +1,54 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Screen1 from '../screen/Screen1';
 import Screen2 from '../screen/Screen2';
 import Screen3 from '../screen/Screen3';
 import React from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
 import Screen4 from '../screen/Screen4';
+import BoardWtrite from '../screen/board/write/boardWrite.presenter';
 
 const Tab = createBottomTabNavigator();
+const HomeStack = createNativeStackNavigator();
+const MapStack = createNativeStackNavigator();
+const PickedStack = createNativeStackNavigator();
+const MypageStack = createNativeStackNavigator();
 // const HomeStack = creacteStackNavigator();
+
+const HomeStackScreen = () => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name="Home"
+        component={Screen1}
+        options={{
+          title: 'Home',
+          headerShown: false,
+        }}
+      />
+      <HomeStack.Screen
+        name="Write"
+        component={BoardWtrite}
+        options={{
+          title: '동행찾기 글쓰기',
+          // headerShown: false
+        }}
+      />
+    </HomeStack.Navigator>
+  );
+};
+const MapStackScreen = () => {
+  return (
+    <MapStack.Navigator>
+      <MapStack.Screen
+        name="Map"
+        component={Screen2}
+        options={{title: 'Map', headerShown: false}}
+      />
+    </MapStack.Navigator>
+  );
+};
+
 export default function Tabs() {
   return (
     <Tab.Navigator
@@ -15,10 +56,10 @@ export default function Tabs() {
         tabBarIcon: ({focused, color, size}) => {
           let iconName;
 
-          if (route.name === 'Home') {
+          if (route.name === 'Homestack') {
             iconName = focused ? 'home' : 'home-outline';
           }
-          if (route.name === 'Map') {
+          if (route.name === 'Mapstack') {
             iconName = focused ? 'map' : 'map-outline';
           }
           if (route.name === 'Picked') {
@@ -43,8 +84,8 @@ export default function Tabs() {
           borderTopRightRadius: 30,
         },
       })}>
-      <Tab.Screen name="Home" component={Screen1} />
-      <Tab.Screen name="Map" component={Screen2} />
+      <Tab.Screen name="Homestack" component={HomeStackScreen} />
+      <Tab.Screen name="Mapstack" component={MapStackScreen} />
       <Tab.Screen name="Picked" component={Screen3} />
       <Tab.Screen name="MyPage" component={Screen4} />
     </Tab.Navigator>
