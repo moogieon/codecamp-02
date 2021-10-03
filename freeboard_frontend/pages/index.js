1494319b2bc2ace270941b815850af670cbf4920
moogieon <vzlzydn5007@gmail.com>
2021-10-03 20:42:14 +0900

    build test

--- freeboard_frontend/pages/index.js
+++ freeboard_frontend/pages/index.js
@@ -1,7 +1,15 @@
 // import Image from 'next/image'
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import BoardListPage from "./boards/index";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/boards/");
+  }, []);
+
   return (
     <>
       <BoardListPage />
