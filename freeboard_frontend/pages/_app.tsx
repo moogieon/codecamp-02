3914aacbda2d510f5f5498b8af8f8297887a6850
moogieon <vzlzydn5007@gmail.com>
2022-11-02 00:01:20 +0900

    getsttaciprops 삭제, 전체 수정

--- freeboard_frontend/pages/_app.tsx
+++ freeboard_frontend/pages/_app.tsx
@@ -78,7 +78,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   });
   const uploadLink = createUploadLink({
     // uri: "https://backend06.codebootcamp.co.kr/graphql",
-    uri: "https://backend08.codebootcamp.co.kr/graphql",
+    uri: "https://backend08.codebootcamp.co.kr/graphql22",
     headers: {
       authorization: `Bearer ${accessToken}`,
     },
