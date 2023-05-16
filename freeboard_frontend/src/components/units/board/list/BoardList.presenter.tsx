cb4a695d294c351099205addbdfcd213f2e06f74
moogieon <vzlzydn5007@gmail.com>
2023-05-16 20:17:29 +0900

    css 수정, 기타 데이터 수정

--- freeboard_frontend/src/components/units/board/list/BoardList.presenter.tsx
+++ freeboard_frontend/src/components/units/board/list/BoardList.presenter.tsx
@@ -25,58 +25,70 @@ export default function BoardListUi(props: any) {
   return (
     <>
       <Wrapper>
-        <Body>
-          <Searchbars01
-            refetch={props.refetch}
-            onChangeKeyword={props.onChangeKeyword}
-          />
-          <Wrapper_Head>
-            <Board_Num>번호</Board_Num>
-            <Board_Title>제목</Board_Title>
-            <Board_Writer>작성자</Board_Writer>
-            <Board_Date>날짜</Board_Date>
-          </Wrapper_Head>
-          {props.onData?.fetchBoards.map((data: any, index: any) => (
-            <Wrapper_Body
-              key={data._id}
-              onClick={props.onClickMoveToBoardDetail(data._id)}
-            >
-              <Column_Num>{index + 1}</Column_Num>
-              <Column_Title id={data._id}>
-                {data.title
-                  .replaceAll(props.keyword, `@#$%${props.keyword}@#$%`)
-                  .split("@#$%")
+        {props.onData?.fetchBoards.length ? (
+          <>
+            <Body>
+              <Searchbars01
+                refetch={props.refetch}
+                onChangeKeyword={props.onChangeKeyword}
+              />
+              <Wrapper_Head>
+                <Board_Num>번호</Board_Num>
+                <Board_Title>제목</Board_Title>
+                <Board_Writer>작성자</Board_Writer>
+                <Board_Date>날짜</Board_Date>
+              </Wrapper_Head>
+              {props.onData?.fetchBoards.map((data: any, index: any) => (
+                <Wrapper_Body
+                  key={data._id}
+                  onClick={props.onClickMoveToBoardDetail(data._id)}
+                >
+                  <Column_Num>{index + 1}</Column_Num>
+                  <Column_Title id={data._id}>
+                    {data.title
+                      .replaceAll(props.keyword, `@#$%${props.keyword}@#$%`)
+                      .split("@#$%")
 
-                  .map((data: string) => (
-                    <Word key={uuidv4()} isMatched={props.keyword === data}>
-                      {data}
-                    </Word>
-                  ))}
-              </Column_Title>
-              <Column_Writer>{data.writer}</Column_Writer>
-              <Column_Date>
-                {new Date(data.createdAt)
-                  .getFullYear()
-                  .toString()
-                  .substring(2, 4)}
-                -
-                {String(new Date(data.createdAt).getMonth() + 1).padStart(
-                  2,
-                  "0"
-                )}
-                -
-                {new Date(data.createdAt).getDate().toString().padStart(2, "0")}
-              </Column_Date>
-            </Wrapper_Body>
-          ))}
-        </Body>
+                      .map((data: string) => (
+                        <Word key={uuidv4()} isMatched={props.keyword === data}>
+                          {data}
+                        </Word>
+                      ))}
+                  </Column_Title>
+                  <Column_Writer>{data.writer}</Column_Writer>
+                  <Column_Date>
+                    {new Date(data.createdAt)
+                      .getFullYear()
+                      .toString()
+                      .substring(2, 4)}
+                    -
+                    {String(new Date(data.createdAt).getMonth() + 1).padStart(
+                      2,
+                      "0"
+                    )}
+                    -
+                    {new Date(data.createdAt)
+                      .getDate()
+                      .toString()
+                      .padStart(2, "0")}
+                  </Column_Date>
+                </Wrapper_Body>
+              ))}
+            </Body>
+          </>
+        ) : (
+          <Body>게시물이 없습니다.</Body>
+        )}
         <Footer>
-          <Paginations01
-            count={props.dataBoardsCount?.fetchBoardsCount}
-            startPage={props.startPage}
-            setStartPage={props.setStartPage}
-            refetch={props.refetch}
-          />
+          {props.onData?.fetchBoards.length && (
+            <Paginations01
+              count={props.dataBoardsCount?.fetchBoardsCount}
+              startPage={props.startPage}
+              setStartPage={props.setStartPage}
+              refetch={props.refetch}
+            />
+          )}
+
           <Button onClick={props.onClickMoveToBoardNew}>
             <Write>게시물 작성</Write>
             <Penimg src="/boards-image/pen.png" />
